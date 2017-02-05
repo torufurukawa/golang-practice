d451e4b5798c2a53b78ab1b7e55011e1427da1b2
Toru Furukawa <torufurukawa@gmail.com>
2017-02-05 21:43:24 +0900

    Add discarding

--- bufio-practice.go
+++ bufio-practice.go
@@ -7,10 +7,15 @@ import (
 )
 
 func main() {
-	// read 3 bytes
 	data := []byte("hello")
 	reader := bufio.NewReader(bytes.NewReader(data))
 	b := make([]byte, 3)
 	reader.Read(b)
-	fmt.Println(b)
+	fmt.Println(string(b)) // "hel"
+
+	reader.Discard(1)
+
+	b = make([]byte, 3)
+	reader.Read(b)
+	fmt.Println(string(b)) // "o"
 }
