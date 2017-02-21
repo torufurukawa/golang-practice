0f1e7f3b160cf9803e08c39993f2cd736e528d87
Toru Furukawa <torufurukawa@gmail.com>
2017-02-21 14:37:58 +0900

    d

--- flag-practice.go
+++ flag-practice.go
@@ -3,11 +3,27 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
+
+// go run flag-practice.go show --foo=chinko
 
 func main() {
 	foo := ""
-	flag.StringVar(&foo, "foo", "DEFAULT", "foo parameter")
-	flag.Parse()
-	fmt.Println(foo)
+	showCommand := flag.NewFlagSet("show", flag.ExitOnError)
+	showCommand.StringVar(&foo, "foo", "DEFAULT", "foo param")
+
+	if len(os.Args) == 1 {
+		fmt.Println("go run flag-practice.go <COMMAND> [--foo=<FOO>]")
+		return
+	}
+
+	switch os.Args[1] {
+	case "show":
+		showCommand.Parse(os.Args[2:])
+		fmt.Println(foo)
+	default:
+		fmt.Printf("%q is not valid command.\n", os.Args[1])
+		os.Exit(2)
+	}
 }
