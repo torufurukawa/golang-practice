f94858cd6045cbc240e4bfab6c402728f9f73ae4
Toru Furukawa <torufurukawa@gmail.com>
2017-02-23 22:24:48 +0900

    d

--- ch-practice.go
+++ ch-practice.go
@@ -4,6 +4,25 @@ import "fmt"
 import "time"
 
 func main() {
+	done := false
+	go do(&done)
+
+	for {
+		fmt.Println(done)
+		if done == true {
+			break
+		}
+		time.Sleep(time.Second * 1)
+	}
+}
+
+func do(done *bool) {
+	fmt.Println("will do")
+	*done = true
+	fmt.Println(done)
+}
+
+func oldmain() {
 	done := make(chan bool)
 	go handle(done)
 	time.Sleep(time.Second * 1)
