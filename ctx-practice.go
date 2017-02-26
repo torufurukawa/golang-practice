c74540603cd78f5db8912acdc7d1d0ecb72f8ab8
Toru Furukawa <torufurukawa@gmail.com>
2017-02-26 22:46:58 +0900

    d

--- ctx-practice.go
+++ ctx-practice.go
@@ -9,14 +9,17 @@ func main() {
 	fmt.Println("hello")
 	reqCh := make(chan string)
 	respCh := make(chan string)
+	complete := false
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	ctx = context.WithValue(ctx, "reqCh", reqCh)
 	ctx = context.WithValue(ctx, "respCh", respCh)
+	ctx = context.WithValue(ctx, "complete", &complete)
 	go serve(ctx)
 
 	reqCh <- "foo"
 	fmt.Println("resp:", <-respCh)
+	fmt.Println("complete:", complete)
 	cancel()
 	time.Sleep(time.Millisecond * 100)
 }
@@ -27,6 +30,8 @@ func serve(ctx context.Context) {
 		select {
 		case s := <-ctx.Value("reqCh").(chan string):
 			fmt.Println("req:", s)
+			complete := ctx.Value("complete").(*bool)
+			*complete = true
 			ctx.Value("respCh").(chan string) <- strings.ToUpper(s)
 		case <-ctx.Done():
 			fmt.Println("done")
