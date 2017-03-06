43b5634d602a73458f1d80bd1c51aed144b24548
Toru Furukawa <torufurukawa@gmail.com>
2017-03-07 08:29:43 +0900

    d

--- log-prac.go
+++ log-prac.go
@@ -1,8 +1,7 @@
 package main
 
-import (
-	"log"
-)
+import "log"
+import "debug"
 
 func noopln(v ...interface{})               {}
 func noopf(format string, v ...interface{}) {}
@@ -11,12 +10,15 @@ var Debugln func(v ...interface{}) = noopln
 var Debugf func(format string, v ...interface{}) = noopf
 
 func main() {
-	Debugln("foo", "bar", 123)
-	Debugf("%d %v", 123, map[int]string{1: "foo"})
+
+	debug.Println("foo", "bar", 123)
+	debug.Printf("%d %v", 123, map[int]string{1: "foo"})
 
 	Debugln = log.Println
 	Debugf = log.Printf
 
 	Debugln("foo", "bar", 123)
 	Debugf("%d %v", 123, map[int]string{1: "foo"})
+
+	log.Printf = noopf
 }
